340809e174e3ec55a8e2d9cd82a13370712365fd
Pablo Gomez <pablogomez0430@gmail.com>
2024-06-16 22:38:47 -0300

    add layout dashboard

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,49 +2,17 @@ import { z } from 'zod'
 import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import { userAgent } from 'next/server'
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app
-  .get('/hello', (c) => {
-    return c.json({
-      message: 'Hello Next.js!',
-    })
+app.get('/hello', (c) => {
+  return c.json({
+    hello: 'Hello Next.js!',
   })
-  .get(
-    '/hello/:test',
-    zValidator(
-      'param',
-      z.object({
-        test: z.string(),
-      })
-    ),
-    (c) => {
-      const { test } = c.req.valid('param')
-      return c.json({
-        message: `Hello`,
-        test: test,
-      })
-    }
-  )
-  .post(
-    '/create/:postId',
-    zValidator(
-      'json',
-      z.object({
-        name: z.string(),
-        userId: z.string(),
-      })
-    ),
-    zValidator('param', z.object({ postId: z.number() })),
-    (c) => {
-      const { name, userId } = c.req.valid('json')
-      return c.json({})
-    }
-  )
+})
 
 export const GET = handle(app)
 export const POST = handle(app)
